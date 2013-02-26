ddbbb8e7b6ba95cc92bfba0077461725b0157f65
Dennis McRitchie <dmcr@princeton.edu>
2013-02-26 18:30:49 +0000

    Test file and minor edits.

--- mturk/list_mturk_hits.py
+++ mturk/list_mturk_hits.py
@@ -3,17 +3,19 @@ from MTurkMappingAfrica import MTurkMappingAfrica
 
 mtma = MTurkMappingAfrica()
 
-#hits = mtma.mtcon.get_all_hits(response_groups='HITDetail, Minimal, HITAssignmentSummary')
-hits = mtma.mtcon.get_all_hits()
+hits = mtma.getAllHits()
 nh = 0
+nah = 0
 print "HIT Id, status, #assign rem, #assign compl, #assign pend, title"
 print "Assign Ids"
 for hit in hits:
         nh = nh + 1
+        if hit.HITStatus == 'Assignable':
+            nah = nah + 1
         print hit.HITId, hit.HITStatus, hit.NumberOfAssignmentsAvailable, \
             hit.NumberOfAssignmentsCompleted, hit.NumberOfAssignmentsPending, \
             hit.Title
         asmts = mtma.mtcon.get_assignments(hit_id=hit.HITId)
         for asmt in asmts:
             print asmt.AssignmentId
-print nh, 'HITs'
+print 'Total HITs: %d; # assignable HITs: %d' % (nh, nah)
