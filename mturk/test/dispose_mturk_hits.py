c8b90f8c5ecd1197b1256e8a26afacaaa7b17173
Dennis McRitchie <dmcr@princeton.edu>
2013-03-20 18:29:26 +0000

    Improved test utility script.

--- mturk/test/dispose_mturk_hits.py
+++ mturk/test/dispose_mturk_hits.py
@@ -1,15 +1,13 @@
-from boto.mturk.connection import MTurkRequestError
 from MTurkMappingAfrica import MTurkMappingAfrica
 
 mtma = MTurkMappingAfrica()
 
-hits = mtma.mtcon.get_all_hits()
+hits = mtma.getAllHits()
 nh = 0
 for hit in hits:
         nh = nh + 1
         print hit.HITId, hit.HITStatus, hit.Title
-        if hit.HITStatus == 'Assignable':
-            mtma.mtcon.expire_hit(hit.HITId)
-            mtma.mtcon.dispose_hit(hit.HITId)
-            print "Expired and deleted HITId %s" % hit.HITId
+        if hit.HITStatus == 'Reviewable':
+            mtma.disposeHit(hit.HITId)
+            print "Deleted HITId %s" % hit.HITId
 print nh, 'HITs'
