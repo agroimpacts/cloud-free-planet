b0fcdfb203417fd00749758d22ac5bf3860e1052
Dennis McRitchie <dmcr@princeton.edu>
2013-09-20 12:46:40 +0000

    Fixed a couple of bugs with the database backup scripts.

--- pgsql/daily_backup.sh
+++ pgsql/daily_backup.sh
@@ -7,12 +7,14 @@ SDIR=`dirname $0`
 IFS='/'
 array=($0)
 user=${array[2]}
+IFS=' '
 
 if [ "$user" == "mapper" ]; then
     dbname="SouthAfrica"
 elif [ "$user" == "sandbox" ]; then
     dbname="SouthAfricaSandbox"
 else
+    echo "$0 must be run using /u/mapper or /u/sandbox path"
     exit 1
 fi
 $SDIR/dump_schema.sh $dbname
