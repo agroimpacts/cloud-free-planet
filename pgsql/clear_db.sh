9a681768d0590d8fa27b2d044fc2bb116cc91d43
Dennis McRitchie <dmcr@princeton.edu>
2013-12-24 17:16:18 +0000

    Modify clear_db.sh script to be conformant with schema modifications.

--- pgsql/clear_db.sh
+++ pgsql/clear_db.sh
@@ -8,19 +8,17 @@ else
     echo "$0 must be run as user mapper or user sandbox"
     exit 1
 fi
+# Delete the HIT and HIT-related data in all tables in the required order, except:
+# preserve worker list and status in worker_data and qual_worker_data.
+# Mark all QAQC and non-QAQC KMLs as available.
 psql -U ***REMOVED*** $dbname <<EOD
 delete from user_maps;
 delete from error_data;
 delete from assignment_data;
-delete from worker_data;
-delete from hit_data;
 delete from qual_user_maps;
 delete from qual_error_data;
 delete from qual_assignment_data;
-delete from qual_worker_data;
-update system_data set value=0 where key='CurNonQaqcGid';
+delete from hit_data;
 update system_data set value=0 where key='CurQaqcGid';
-update system_data set value=0 where key='SumPendingNonQaqcs';
-update system_data set value=0 where key='NumQaqcs';
-update system_data set value=0 where key='AvgPendingNonQaqcs';
+update kml_data set mapped = false;
 EOD
--- pgsql/daily_backup.sh
+++ pgsql/daily_backup.sh
@@ -3,7 +3,7 @@
 # Child scripts are in the same directory
 SDIR=`dirname $0`
 
-# Assumes we were called with '/u/${USER}/afmap/pgsql/<script_name'.
+# Assumes we were called with '/u/${USER}/afmap/pgsql/<script_name>'.
 IFS='/'
 array=($0)
 user=${array[2]}
