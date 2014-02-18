667a3f31006be713904c41980c5b17537eb18194
Lyndon Estes <lestes@princeton.edu>
2014-02-18 19:37:06 +0000

    Update mapFix.py to remove multiple conditional statements in tests for qual_user_maps versus user_maps. Also added in appropriate SQL statements to select for assignment/training_id when updating, as this would have caused over-writing errors.

--- mturk/test/mapFix.py
+++ mturk/test/mapFix.py
@@ -10,26 +10,14 @@ if(len(sys.argv) < 4 or len(sys.argv) > 6):
     print "Error: There must be at least 5 and no more than 6 arguments: maptype, kmlid, assn id, testing option, and/or try id"
     quit()
 
-# ma 24DF395SW6NG74QIZNEJEYF1HHKFTN
 mtype = sys.argv[1]
 kmlid = sys.argv[2]
 assignmentid = sys.argv[3]
 testing = sys.argv[4] 
 if(len(sys.argv) == 6): 
     tryid = sys.argv[5]
-elif(len(sys.argv) == 5):
+else: 
     tryid = 'NA'
-
-
-# Specify whether qual or production maps being cleaned
-if mtype == 'ma':
-    map_table = 'user_maps'
-    assignment_type = 'assignment_id'
-    trystr = ""
-elif mtype == 'tr':
-    map_table = 'qual_user_maps'
-    assignment_type = 'training_id'
-    trystr = ',try'
 
 mtma = MTurkMappingAfrica()
 
@@ -50,11 +38,11 @@ if (testing == "yes" or testing == "comments"):
     print fixshpnm1, fixshpnm2
 
 if mtype == "tr":
-    mtma.cur.execute("""SELECT name,ST_AsEWKT(geom)%s from %s where name ~ '%s' and  %s='%s' and try='%s' 
-        order by name for update""" % (trystr, map_table, kmlid, assignment_type, assignmentid, tryid))
+    mtma.cur.execute("""SELECT name,ST_AsEWKT(geom),try FROM qual_user_maps WHERE name LIKE '%s_%%' AND training_id='%s' AND 
+        try='%s' ORDER BY name FOR UPDATE""" % (kmlid, assignmentid, tryid))
 elif mtype == "ma":
-    mtma.cur.execute("""SELECT name,ST_AsEWKT(geom)%s from %s where name ~ '%s' and %s='%s' order by name for update"""
-        % (trystr, map_table, kmlid, assignment_type, assignmentid))
+    mtma.cur.execute("""SELECT name,ST_AsEWKT(geom) FROM user_maps WHERE name LIKE '%s_%%' AND assignment_id='%s' ORDER BY name 
+        FOR UPDATE""" % (kmlid, assignmentid))
 poly_table = mtma.cur.fetchall()
 
 mp.polyPrepair(fixshpnm1, poly_table)
@@ -64,15 +52,14 @@ poly_fix2 = mp.cleanPolyHash(poly_fix)
 mp.shapeWriter(poly_fix2, fixshpnm3) 
 mp.polyPPrepair(fixshpnm3, fixshpnm4)
 poly_fixf = mp.shapeReader(fixshpnm4)
-if(mtma.cur.rowcount == len(poly_fixf)): 
-    #length_match = "TRUE" 
+if(len(poly_table) == len(poly_fixf)): 
     for name in poly_fixf:
         if mtype == "tr":
-            update_str = """UPDATE %s SET geom_clean = ST_GeomFromEWKT('SRID=4326;%s')
-                WHERE name = '%s' and try='%s'""" % (map_table, poly_fixf[name][0], name, tryid)
+            update_str = """UPDATE qual_user_maps SET geom_clean = ST_GeomFromEWKT('SRID=4326;%s')
+                WHERE name = '%s' AND training_id='%s' AND try='%s'""" % (poly_fixf[name][0], name, assignmentid, tryid)
         elif mtype == "ma":
-            update_str = """UPDATE %s SET geom_clean = ST_GeomFromEWKT('SRID=4326;%s') 
-                WHERE name = '%s'""" % (map_table, poly_fixf[name][0], name)
+            update_str = """UPDATE user_maps SET geom_clean = ST_GeomFromEWKT('SRID=4326;%s') 
+                WHERE name = '%s' AND assignment_id='%s'""" % (poly_fixf[name][0], name, assignmentid)
         mtma.cur.execute(update_str)
         if (testing == "yes" or testing == "comments"):
             print update_str
