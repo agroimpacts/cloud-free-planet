45099c3511cd1010ffb257d875b0dc72de0360cd
Dennis McRitchie <dmcr@princeton.edu>
2015-02-06 16:55:44 +0000

    Committing new build of prepair and pprepair.

---
+++ pprepair/CMakeFiles/pprepair.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/u/sandbox/afmap/pprepair/FaceInfo.cpp" "/u/sandbox/afmap/pprepair/CMakeFiles/pprepair.dir/FaceInfo.cpp.o"
+  "/u/sandbox/afmap/pprepair/IOWorker.cpp" "/u/sandbox/afmap/pprepair/CMakeFiles/pprepair.dir/IOWorker.cpp.o"
+  "/u/sandbox/afmap/pprepair/PlanarPartition.cpp" "/u/sandbox/afmap/pprepair/CMakeFiles/pprepair.dir/PlanarPartition.cpp.o"
+  "/u/sandbox/afmap/pprepair/PolygonHandle.cpp" "/u/sandbox/afmap/pprepair/CMakeFiles/pprepair.dir/PolygonHandle.cpp.o"
+  "/u/sandbox/afmap/pprepair/pprepair.cpp" "/u/sandbox/afmap/pprepair/CMakeFiles/pprepair.dir/pprepair.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  "/usr/local/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
