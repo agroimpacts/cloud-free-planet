64b114677f2ae9cfbbaa87560b5739bf14a2a01d
Dennis McRitchie <dmcr@princeton.edu>
2018-02-22 16:42:31 -0500

    Initial commit after removing Mturk dependencies.

---
+++ common/mapCleaners.py
@@ -0,0 +1,123 @@
+# functions for cleaning mapping polygons
+import os, re, subprocess, glob
+from osgeo import ogr
+from datetime import datetime
+from MappingCommon import MappingCommon
+
+def polyPrepair(mapc, shape_name, table):
+    shape_name_out = shape_name + ".shp"
+    #print shape_name, shape_name_out, os.getcwd()
+    driver = ogr.GetDriverByName('ESRI Shapefile')
+    if os.path.exists(shape_name_out):
+        driver.DeleteDataSource(shape_name_out)
+        
+    ds = driver.CreateDataSource(shape_name_out)
+    layer = ds.CreateLayer(shape_name, geom_type=ogr.wkbPolygon)
+    layer_defn = layer.GetLayerDefn()
+    fd = ogr.FieldDefn('name', ogr.OFTString)
+    layer.CreateField(fd)
+
+    for i in range(0, len(table)):
+        name = table[i][0]
+        wkt = re.sub("^SRID=*.*;", "", table[i][1])
+        #wktfix = subprocess.Popen([mapc.projectRoot + "/prepair/prepair", "--wkt", wkt, "--minarea", "0.000000001"],
+        #    stdout=subprocess.PIPE).communicate()[0]
+        wktfix = wktPrepair(mapc, wkt)
+        geom = ogr.CreateGeometryFromWkt(wktfix)
+        featureIndex = i
+        feature = ogr.Feature(layer_defn)
+        feature.SetGeometry(geom)
+        feature.SetFID(featureIndex)
+        feature.SetField('name', name)
+        layer.CreateFeature(feature)
+        feature.Destroy()
+    
+    ds.Destroy()
+
+def wktPrepair(mapc, wkt):
+    wktfix = subprocess.Popen([mapc.projectRoot + "/prepair/prepair", "--wkt", wkt, "--minarea", "0.000000001"],
+        stdout=subprocess.PIPE).communicate()[0]
+    return(wktfix)
+
+def polyPPrepair(mapc, shape_name_in, shape_name_out):
+    shape_in = shape_name_in + ".shp"
+    shape_out = shape_name_out + ".shp"
+    polyfix = subprocess.Popen([mapc.projectRoot + "/pprepair/pprepair", "-i", shape_in, "-o", shape_out, "-fix"], 
+        stdout=subprocess.PIPE).communicate()[0]
+
+def shapeReader(mapc, shape_name_in):
+    logFilePath = mapc.projectRoot + "/log"
+    shape_in = shape_name_in + ".shp"
+    now = str(datetime.today())
+    polys = ogr.Open(shape_in)
+    if polys is None:
+        k = open(logFilePath + "/miscellaneous.log", "a")
+        k.write("mapFix: OGR couldn't open '%s' at '%s'\n" % (shape_in, now))
+        k.close()
+        raise Exception("OGR can't open '%s'!" % shape_in)
+    elements = polys.GetLayer()
+    outHash = {}
+
+    for element in elements:
+        name = element.GetField("name")
+        geom = element.GetGeometryRef()
+        area = geom.GetArea()
+        wkt = geom.ExportToWkt()
+        if name not in outHash:
+            outHash[name] = [wkt]
+        else: 
+            outHash[name].append(wkt)
+    return(outHash)
+
+def shapeWriter(mapc, poly_hash, shape_name_out):
+    shape_name = shape_name_out + ".shp"
+    driver = ogr.GetDriverByName('ESRI Shapefile')
+    if os.path.exists(shape_name):
+        driver.DeleteDataSource(shape_name)
+
+    ds = driver.CreateDataSource(shape_name)
+    layer = ds.CreateLayer(shape_name, geom_type=ogr.wkbPolygon)
+    layer_defn = layer.GetLayerDefn()
+    fd = ogr.FieldDefn('name', ogr.OFTString)
+    layer.CreateField(fd)
+    fids = 0
+    for name in poly_hash:
+        fids += 1
+        geom = ogr.CreateGeometryFromWkt(poly_hash[name][0])
+        featureIndex = fids
+        feature = ogr.Feature(layer_defn)
+        feature.SetGeometry(geom)
+        feature.SetFID(featureIndex)
+        feature.SetField('name', name)
+        layer.CreateFeature(feature)
+        feature.Destroy()
+
+    ds.Destroy()
+
+def cleanPolyHash(mapc, poly_hash):
+    cleanHash = {} 
+    for name in poly_hash:
+        if len(poly_hash[name]) == 1:
+            wktfix = wktPrepair(mapc, poly_hash[name][0])
+            cleanHash[name] = [wktfix]
+        else:
+            poly1 = ogr.CreateGeometryFromWkt(poly_hash[name][0])
+            for polygon in poly_hash[name][1:]:
+                geom = ogr.CreateGeometryFromWkt(polygon)
+                union = poly1.Union(geom)                
+            unionwkt = union.ExportToWkt()
+            unionwkt_fix = wktPrepair(mapc, unionwkt)
+            cleanHash[name] = [unionwkt_fix] 
+            
+    return(cleanHash)
+
+def tempShapeDel(mapc, path, nameroot):
+    tempfilelist = glob.glob(path + "*" + nameroot + "*")
+    try:
+        for f in tempfilelist:
+            #print(f)
+            os.remove(f)
+    except Exception,e:
+        print e
+
+
---
+++ common/mapFix.py
@@ -0,0 +1,87 @@
+#! /usr/bin/python
+# Cleans polygons in database
+
+from osgeo import ogr
+import glob, sys, os, subprocess, re #getpass
+from MappingCommon import MappingCommon
+import mapCleaners as mp
+
+def mapFix(mapc, mtype, kmlid, assignmentid, testing, tryid=None):
+    cleaning_dir = mapc.projectRoot + "/laundromat/"  # directory for temporary maps
+
+    fixshpnm = assignmentid.encode('utf-8')
+    if tryid is not None:
+        fixshpnm = "%s_%s" % (fixshpnm, tryid)
+    fixshpnm1 = cleaning_dir + "temp_" + fixshpnm
+    fixshpnm2 = cleaning_dir + "temp_fix_" + fixshpnm
+    fixshpnm3 = cleaning_dir + "temp_fix_2_" + fixshpnm
+    fixshpnm4 = cleaning_dir + "temp_fix_3_" + fixshpnm
+
+    if (testing == "yes" or testing == "comments"):
+        print "Effective user: " + mapc.euser
+        print "Root directory: " + mapc.projectRoot
+        print "Map cleaning directory: " + os.getcwd()
+        print "Assignment ID: " + assignmentid
+        if tryid is not None:
+            print "Try ID: %s" % tryid
+
+    if mtype == "tr" and tryid is not None:
+        mapc.cur.execute("""SELECT name,ST_AsEWKT(geom),try FROM qual_user_maps WHERE name LIKE '%s_%%' AND worker_id='%s' AND 
+            try='%s' ORDER BY name FOR UPDATE""" % (kmlid, assignmentid, tryid))
+    elif mtype == "ma" and tryid is None:
+        mapc.cur.execute("""SELECT name,ST_AsEWKT(geom) FROM user_maps WHERE name LIKE '%s_%%' AND assignment_id='%s' ORDER BY name 
+            FOR UPDATE""" % (kmlid, assignmentid))
+    else:
+        raise Exception ("Invalid maptype or tryid passed to Mapfix")
+
+    poly_table = mapc.cur.fetchall()
+    if len(poly_table) == 0:
+        raise Exception ("Invalid kmlid, assignmentid/trainingid, or tryid passed to Mapfix")
+
+    mp.polyPrepair(mapc, fixshpnm1, poly_table)
+    mp.polyPPrepair(mapc, fixshpnm1, fixshpnm2)
+    poly_fix = mp.shapeReader(mapc, fixshpnm2)
+    poly_fix2 = mp.cleanPolyHash(mapc, poly_fix)
+    mp.shapeWriter(mapc, poly_fix2, fixshpnm3) 
+    mp.polyPPrepair(mapc, fixshpnm3, fixshpnm4)
+    poly_fixf = mp.shapeReader(mapc, fixshpnm4)
+    if(len(poly_table) == len(poly_fixf)): 
+        for name in poly_fixf:
+            if mtype == "tr":
+                update_str = """UPDATE qual_user_maps SET geom_clean = ST_GeomFromEWKT('SRID=4326;%s')
+                    WHERE name = '%s' AND training_id='%s' AND try='%s'""" % (poly_fixf[name][0], name, assignmentid, tryid)
+            elif mtype == "ma":
+                update_str = """UPDATE user_maps SET geom_clean = ST_GeomFromEWKT('SRID=4326;%s') 
+                    WHERE name = '%s' AND assignment_id='%s'""" % (poly_fixf[name][0], name, assignmentid)
+            mapc.cur.execute(update_str)
+            if (testing == "yes" or testing == "comments"):
+                print update_str
+        mapc.dbcon.commit()
+    else: 
+        print "Error: The number of cleaned polygons doesn't match the number of input polygons for assignment" + assignmentid
+
+    if (testing == "no" or testing == "comments"):
+        mp.tempShapeDel(mapc, cleaning_dir, fixshpnm)
+
+    del mapc
+
+def main():
+    if len(sys.argv) < 5 or len(sys.argv) > 6:
+        print "Error: There must be at least 4 and no more than 5 arguments: maptype, kmlid, assn id, testing option, and/or try id"
+        quit()
+
+    mtype = sys.argv[1]
+    kmlid = sys.argv[2]
+    assignmentid = sys.argv[3]
+    assignmentid = unicode(assignmentid, "UTF-8")
+    testing = sys.argv[4]
+    mapc = MappingCommon()
+    if len(sys.argv) == 6:
+        tryid = int(sys.argv[5])
+        mapFix(mapc, mtype, kmlid, assignmentid, testing, tryid)
+    else:
+        mapFix(mapc, mtype, kmlid, assignmentid, testing)
+
+
+if __name__ == '__main__':
+    main()
