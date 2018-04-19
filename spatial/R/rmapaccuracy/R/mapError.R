68420b55797a8c74b1d36854621a37f238fe8e2d
ldemaz <3401881+ldemaz@users.noreply.github.com>
2018-04-18 21:43:48 -0400

    Bug fix in KMLAccuracy/mapError, in case 2 maps, where user maps outside of Q where no fields exist.  mapError updated to add 4th case

--- spatial/R/rmapaccuracy/R/mapError.R
+++ spatial/R/rmapaccuracy/R/mapError.R
@@ -6,26 +6,31 @@
 #' @return Accuracy measures from user maps
 #' @export
 mapError <- function(maps, truth, region) {
-  if(is.null(truth)) {
+  if(is.null(truth) & !is.null(maps)) {  # Yes U, no Q
     null <- region  # Actual null area is whole region
     tp <- 0  # True positive area is 0
     fp <- maps  # False positive area is all of maps
     fn <- 0  # No false negative area because there are no fields
     tn <- st_difference(null, maps)  # True negative: do poly diff across IDs
-  } 
-  if(is.null(maps)) {
-    null <- st_buffer(st_buffer(st_difference(region, truth),0.00001),-0.00001) # actual null region in map
+  } else if(!is.null(truth) & is.null(maps)) {  # No U, Yes Q
+    null <- st_buffer(st_buffer(st_difference(region, truth), 0.00001), 
+                      -0.00001) # actual null region in map
     tp <- 0  # No user maps, no true positive
     fp <- 0  # No user maps, no false positives
     fn <- truth  # False negative area is all of truth
     tn <- null  # True negative area is null - user gets credit for this area
-  }
-  if(!is.null(truth) & !is.null(maps)) {
-    null <- st_buffer(st_buffer(st_difference(region, truth),0.00001),-0.00001)
+  } else if(!is.null(truth) & !is.null(maps)) {  # Yes U, Yes Q
+    null <- st_buffer(st_buffer(st_difference(region, truth), 0.00001), 
+                      -0.00001)
     tp <- st_intersection(truth, maps)  
     fp <- st_difference(maps, truth)  
     fn <- st_difference(truth, maps)  
     tn <- st_difference(null, maps)
+  } else if(is.null(truth) & is.null(maps)) {  # No U, No Q
+    tp <- 0  # True positive area is 0
+    fp <- 0  # No user maps, no false positives
+    fn <- 0  # No false negative area because there are no fields
+    tn <- region  # True negative: user gets credit for full area
   }
   tflist <- c("tp", "fp", "fn", "tn") 
   areas <- sapply(tflist, function(x) {  
