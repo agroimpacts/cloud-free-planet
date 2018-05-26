fed862ffa3ca970244f74429cf4a0562829d4ccc
ldemaz <3401881+ldemaz@users.noreply.github.com>
2018-05-26 14:40:41 -0400

    Added category and categ_comment read/write to qaqc data for check_worker_assignment. Some reshaping also

--- spatial/R/check_worker_assignment.R
+++ spatial/R/check_worker_assignment.R
@@ -67,10 +67,14 @@ if(test_root == "N") {
   qaqc_polys <- DBI::dbGetQuery(coninfo$con, qaqc_sql)
   qaqc_hasfields <- ifelse(nrow(qaqc_polys) > 0, "Y", "N") 
   if(qaqc_hasfields == "Y") {
-    qaqc_sql <- paste0("select gid, geom_clean",
-                       " from qaqcfields where name=", "'", hits$name, "'")
+    qaqc_sql <- paste0("select gid, name, category, categ_comment, geom_clean",
+                       " from qaqcfields where name=", "'", hits$name, "'", 
+                       " order by gid")
     qaqc_polys <- suppressWarnings(st_read(coninfo$con, query = qaqc_sql, 
                                            geom_column = 'geom_clean'))
+    # reorganize: this won't be necessary if we revise qaqcfields table
+    qaqc_polys <- qaqc_polys %>% 
+      mutate(name = paste0(name, "_", 1:nrow(qaqc_polys))) %>% select(-gid)
   }
   
   # Read in user data
@@ -104,7 +108,7 @@ if(test_root == "N") {
   }
   if(nrow(qaqc_polys) > 0) {  # First convert to geographic coords
     suppressWarnings(qaqc_poly <- qaqc_polys %>% 
-                       transmute(kmlname = paste0(kmlid, "_r")))
+                       select(name, category, categ_comment))
     suppressWarnings(st_write(qaqc_poly, delete_dsn = TRUE, quiet = TRUE, 
                               dsn = paste0(worker_path, "/", kmlid, "_r.kml")))
   }
