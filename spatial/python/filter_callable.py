be821b5a1cc989c6656dc5e48ba2a3887e17e96e
Sitian Xiong <35466069+SitianXiong@users.noreply.github.com>
2018-07-19 16:03:19 -0400

    Add files via upload
    This python file contains a callable function "Cloud_Shadow_Stats()."  The function takes a GeoTiff format file, and returns
    a tuple with two float numbers: cloud percentage and shadow percentage.
        Input parameter:
        in_name    - The full path of a Geotiff format image. e.g., r"D:\test_image\planet.tif"
        cloud_val  - The threshold of cloud in the min image(for more about "min image", see #2 in the following); default = 2500;  
        shadow_val - The threshold of shadow in the max image; default = 1500;
        land_val   - The threshold of land in the Near Infrared image (band 4); defalt = 1000
    
        Output: cloud_perc, shadow_perc
        The output is a tuple with two float numbers:  
        cloud_perc  - cloud pixels percentage in that image, 
        shadow_perc - shadow percentage in that image.
    
    Former-commit-id: e13c100af75f63c34a5318587a599f886b8090e0

--- spatial/python/filter_callable.py
+++ spatial/python/filter_callable.py
@@ -16,7 +16,22 @@ import numpy as np
 from scipy import ndimage
 import sys
 
-def Cloud_Shadow_Stats(in_name):
+def Cloud_Shadow_Stats(in_name, cloud_val = 2500, shadow_val = 1500, land_val = 1000):
+    """
+    Input parameter:
+    in_name    - The full path of a Geotiff format image. e.g., r"D:\test_image\planet.tif"
+    cloud_val  - The threshold of cloud in the min image(for more about "min image", see #2 in the following); default = 2500;  
+    shadow_val - The threshold of shadow in the max image; default = 1500;
+    land_val   - The threshold of land in the Near Infrared image (band 4); defalt = 1000
+
+    Output: cloud_perc, shadow_perc
+    The output is a tuple with two float numbers:  
+    cloud_perc  - cloud pixels percentage in that image, 
+    shadow_perc - shadow percentage in that image.
+    """
+
+
+
     #1 open the tif, take 4 bands, and read them as arrays
     in_ds = gdal.Open(in_name)
     b1 = in_ds.GetRasterBand(1)
@@ -29,7 +44,7 @@ def Cloud_Shadow_Stats(in_name):
     b3_array = b3.ReadAsArray()
     b4_array = b4.ReadAsArray()
 
-    #2. make max image and min image.
+    #2. make max image and min image from four input bands.
     #np.dstack() takes a list of bands and makes a band stack
     #np.amax() find the max along the axis, here 2 means the axis that penetrates through bands in each pixel.
     band_list = [b1_array,b2_array,b3_array,b4_array]
@@ -49,7 +64,7 @@ def Cloud_Shadow_Stats(in_name):
     #The threshold here is based on Sitian and Tammy's test on 11 planet scenes.  It may not welly work for every AOI.
     #Apparently np.where() method will change or lost the datatype, so .astype(np.int16) is used to make sure the datatype is the same as original
     cloud_array = np.where(min7x7_img > 3150, 1, 0).astype(np.int16)
-    shadow_and_water_array = np.where(max7x7_img > 2000, 1, 0).astype(np.int16)
+    shadow_and_water_array = np.where(max7x7_img < 2000, 1, 0).astype(np.int16)
     land_array = np.where(b4_array > 1000, 1, 0).astype(np.int16)
 
     del max7x7_img, min7x7_img, b4_array
@@ -64,10 +79,7 @@ def Cloud_Shadow_Stats(in_name):
 
     cloud_perc = cloud_count/grid_count
     shadow_perc = shadow_count/grid_count
-    print("The total number of pixel in the grid is {}, the cloud perc is :{}, the shadow perc is {}".format(grid_count, cloud_perc, shadow_perc))
 
     del cloud_array, shadow_and_water_array, land_array, shadow_array
     return cloud_perc, shadow_perc
 
-
-Cloud_Shadow_Stats(r"D:\Extracting_Clouds\grid_cells\ZA0121779_20180128_073103_1006_3B_MS_SR_GS.tif")
