2c974d09bdc0ce517e166eb8e34387c4c70f13da
SuYe99 <remotesensingsuy@gmail.com>
2018-08-01 18:04:16 -0400

    Add row and column terms into the naming of outputted consensus maps; Change the harded coded imagery source to an argument that can be passed by; little improvement on category accuracy assessment;
    Former-commit-id: ad4d83099a499049d94810eb41cf44a4c2deaa1b [formerly 3fb731652da3321d77008fe15cc13896c946280a]
    Former-commit-id: ad4d83099a499049d94810eb41cf44a4c2deaa1b
    Former-commit-id: 3bb6d1c50cd370830f7934ed120fb7ecd18ff58d

--- spatial/R/consensus_map_generator.R
+++ spatial/R/consensus_map_generator.R
@@ -19,40 +19,42 @@ suppressMessages(library(rmapaccuracy)) # have to load this to get connection
 # alt.root <- NULL # testlines
 # host <- NULL # testlines
 
-if(length(arg) < 2) stop("At least 2 arguments needed", call. = FALSE)
+if(length(arg) < 3) stop("At least 3 arguments needed", call. = FALSE)
 arg <- commandArgs(TRUE)
-kmlid <- arg[1]  # ID of grid cell 
-min.mappedcount <- arg[2] # minimum mapped map count
-if(length(arg) == 2) {
+kmlid <- arg[1]  # ID of grid cell
+source <- arg[2] # image source, 'wv2' or 'planet'
+min.mappedcount <- arg[3] # minimum mapped map count
+if(length(arg) == 3) {
   output.riskmap <- FALSE
   db.tester.name <- NULL
   alt.root <- NULL
   host <- NULL
 } 
-if(length(arg) > 2) {
-  if(is.na(arg[3])){
+if(length(arg) > 3) {
+  if(is.na(arg[4])){
     output.riskmap <- FALSE
   } else{
-    output.riskmap <- arg[3]
+    output.riskmap <- arg[4]
   }
-  if(is.na(arg[4])){
+  if(is.na(arg[5])){
     db.tester.name <- NULL
   } else {
-    db.tester.name <- arg[4]
+    db.tester.name <- arg[5]
   }
-  if(is.na(arg[5])) {
+  if(is.na(arg[6])) {
     alt.root <- NULL
   } else {
-    alt.root <- arg[5]
+    alt.root <- arg[6]
   } 
-  if(is.na(arg[6])) {
+  if(is.na(arg[7])) {
     host <- NULL
   } else {
-    host <- arg[6]
+    host <- arg[7]
   }
 }
 
-consensus_map_creation(kmlid = kmlid, min.mappedcount = min.mappedcount, 
+consensus_map_creation(kmlid = kmlid, source = source, 
+                       min.mappedcount = min.mappedcount, 
                        output.riskmap = output.riskmap,
                        riskpixelthres  = riskpixelthres, diam = diam, 
                        user = user, password = password, 
