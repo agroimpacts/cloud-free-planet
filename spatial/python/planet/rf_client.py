4f495f6643e3c32578cb083ee952684e068f3e26
Grigory Pomadchin <gr.pomadchin@gmail.com>
2018-08-15 16:59:15 +0300

    Add RFClient dep
    Former-commit-id: 6e3a5d89e98235bc006c7ff4402ea8f78554f2c1

---
+++ spatial/python/planet/rf_client.py
@@ -0,0 +1,11 @@
+from rasterfoundry.api import API
+import logging
+
+class RFClient():
+    def __init__(self, config):
+        rf_config = config['rasterfoundry']
+        self.api_key = rf_config['api_key']
+        self.api_uri = rf_config['api_uri']
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.INFO)
+        self.api = API(refresh_token = self.api_key, host = self.api_uri)
