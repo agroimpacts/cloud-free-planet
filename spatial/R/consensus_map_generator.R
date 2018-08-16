f1c113c70fe94c5c8591f253cd3e0915a46bd34d
SuYe99 <remotesensingsuy@gmail.com>
2018-08-16 15:02:45 -0400

    Change some hard-coded variables to reading from configuration table; update the S3 output structure for consensus generation; use yaml for reading AWS configuration; delete useless arguments in consensus_map_generator, and change MappingCommon and daemon accordinly; fix minor bugs for functions calling db_tools

--- spatial/R/consensus_map_generator.R
+++ spatial/R/consensus_map_generator.R
@@ -3,44 +3,39 @@
 # Author: Su Ye
 
 # Static arguments
-output.heatmap <- FALSE # if output heat map
 diam <- 0.005 / 2 # new master grid diameter
-riskpixelthres <- 0.4 # determine risky pixels that are larger than threshold
 suppressMessages(library(rmapaccuracy)) # have to load this to get connection
 
 # Input args 
 # kmlid <- "GH0067266" # testlines
-# min.mappedcount <- 0 # testlines
-# scorethres <- 0     # testlines
 # output.riskmap <- FALSE # testlines
-# db.tester.name <- 'sye' # testlines
-# alt.root <- NULL # testlines
 # host <- NULL # testlines
 
 arg <- commandArgs(TRUE)
 kmlid <- arg[1]  # ID of grid cell
+
+## legacy of previous version, and may delete in future
 kml.usage <- arg[2] # the use of kml, could be 'train, 'validate' or 'holdout'
-min.mappedcount <- arg[3] # minimum mapped map count
-if(length(arg) < 3) stop("At least 3 arguments needed", call. = FALSE)
-if(length(arg) == 3) {
+
+if(length(arg) < 2) stop("At least 2 arguments needed", call. = FALSE)
+if(length(arg) == 2) {
   output.riskmap <- FALSE
   host <- NULL
 } 
-if(length(arg) > 3) {
-  if(is.na(arg[4])){
+if(length(arg) > 2) {
+  if(is.na(arg[3])){
     output.riskmap <- FALSE
   } else{
-    output.riskmap <- arg[4]
+    output.riskmap <- arg[3]
   }
-  if(is.na(arg[5])) {
+  if(is.na(arg[4])) {
     host <- NULL
   } else {
-    host <- arg[5]
+    host <- arg[4]
   }
 }
 
 consensus_map_creation(kmlid = kmlid, kml.usage = kml.usage, 
-                       min.mappedcount = min.mappedcount, 
                        output.riskmap = output.riskmap,
-                       riskpixelthres = riskpixelthres, diam = diam, 
+                       diam = diam, 
                        host = host)
