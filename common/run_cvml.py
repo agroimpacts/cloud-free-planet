9a027c497c56ede3ba7f76ea652f8676ca89f956
Dennis McRitchie <dmcr@princeton.edu>
2018-08-25 15:45:41 -0400

    Another commit to cleanup sandbox.

--- common/run_cvml.py
+++ common/run_cvml.py
@@ -9,13 +9,6 @@ def main():
     # sets up .terraform/
     mapc = MappingCommon()
     os.chdir(mapc.projectRoot + "/terraform")
-<<<<<<< HEAD
-    tf_init = subprocess.Popen(mapc.projectRoot + "/terraform/terraform init", shell=True, cwd = mapc.projectRoot + "/terraform").wait()
-    # starts cvml cluster and a single iteration
-    os.chdir(mapc.projectRoot + "/terraform")
-    tf_apply =  subprocess.Popen(mapc.projectRoot + "/terraform/terraform apply -auto-approve", shell=True, cwd = mapc.projectRoot + "/terraform").wait()
-    if tf_init == 0 and tf_apply == 0:
-=======
     rf_init = subprocess.Popen(mapc.projectRoot + "/terraform/terraform init", shell=True,
                                cwd=mapc.projectRoot + "/terraform").wait()
     # starts cvml cluster and a single iteration
@@ -24,7 +17,6 @@ def main():
                                 mapc.projectRoot + "/terraform/terraform apply -auto-approve",
                                 cwd=mapc.projectRoot + "/terraform", shell=True).wait()
     if rf_init == 0 and rf_apply == 0:
->>>>>>> a6f7a26240c334c58ac0be6820dbd74484597bd0
         return True
     else:
         return False
