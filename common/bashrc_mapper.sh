8c3675eac14d328121d0627177ba0125a13fa42f
Dennis McRitchie <dmcr@princeton.edu>
2018-08-25 15:30:29 -0400

    Committing some changes to bashrc_mapper.sh and run_cvml.py so I can pull my overlay changes in from devel.

--- common/bashrc_mapper.sh
+++ common/bashrc_mapper.sh
@@ -8,4 +8,4 @@ export TF_VAR_s3_rpm_uri=s3://azavea-africa-test/rpms/4ff6e43910188a1215a1474cd2
 export TF_VAR_s3_notebook_uri=s3://azavea-africa-test/notebooks
 export TF_VAR_bs_bucket=activemapper
 export TF_VAR_bs_prefix=bootstrap-cluster-test
-export PATH="$PATH:~/mapper/terraform"
+export PATH=~/mapperAL/terraform:$PATH
