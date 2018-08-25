a7f00967ae8c93868bb5b4964dda268bc435fd03
LLeiSong <pinot.sung@gmail.com>
2018-08-25 00:47:49 -0400

    Loading environmental variables on the fly for generate_consensus_daemon
    Former-commit-id: fce191754a02b09c5c587b99990dcd39d62e6554 [formerly 3d041c5202a286377308ebbe3b28241fb9618280]
    Former-commit-id: fce191754a02b09c5c587b99990dcd39d62e6554
    Former-commit-id: da239f35982f40bd6d0d19f16e1c7cf5064d4077

--- common/run_cvml.py
+++ common/run_cvml.py
@@ -4,14 +4,22 @@ import os
 from MappingCommon import MappingCommon
 import subprocess
 
+
 def main():
     # sets up .terraform/
     mapc = MappingCommon()
     os.chdir(mapc.projectRoot + "/terraform")
-    subprocess.Popen(mapc.projectRoot + "/terraform/terraform init", shell=True).wait()
+    rf_init = subprocess.Popen(mapc.projectRoot + "/terraform/terraform init", shell=True,
+                               cwd=mapc.projectRoot + "/terraform").wait()
     # starts cvml cluster and a single iteration
-    subprocess.Popen(mapc.projectRoot + "/terraform/terraform apply -auto-approve", shell=True).wait()
-    return True
+    os.chdir(mapc.projectRoot + "/terraform")
+    rf_apply = subprocess.Popen(mapc.projectRoot + "/common/bashrc_mapper.sh ; " +
+                                mapc.projectRoot + "/terraform/terraform apply -auto-approve",
+                                cwd=mapc.projectRoot + "/terraform", shell=True).wait()
+    if rf_init == 0 and rf_apply == 0:
+        return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
