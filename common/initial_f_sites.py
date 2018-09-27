cd019b073b0c8b59b2b725b43156d70b2895fb76
LLeiSong <pinot.sung@gmail.com>
2018-09-27 01:04:22 -0400

    Revert to 0556ed5
    Former-commit-id: 071546bda304b9c82df87e72fdd580b01df4b0de [formerly 9358af3aa4d26a239572d6fc3d0aea5ff700c2a2]
    Former-commit-id: 071546bda304b9c82df87e72fdd580b01df4b0de
    Former-commit-id: b9e2acf7a7e70ca25df6bdb3fb9abc97cafe79a1

--- common/initial_f_sites.py
+++ common/initial_f_sites.py
@@ -3,7 +3,6 @@
 import os
 import psycopg2
 import random
-import sys
 from datetime import datetime as DT
 from MappingCommon import MappingCommon
 
@@ -12,29 +11,18 @@ def main():
     # Connect to the database
     mapc = MappingCommon()
     log_file_path = mapc.projectRoot + "/log"
-    logfname = log_file_path + "/generateSites.log"  # log file name
-    dberrfname = log_file_path + "/sites_dbase_error.log"
     
     # Get the parameters
     n_f = int(mapc.getConfiguration('InitialFnum'))
     pro_hd = float(mapc.getConfiguration('ProportionHoldout'))
     pro_hd1 = float(mapc.getConfiguration('ProportionHoldout1'))
 
-    mapc.cur.execute("SELECT count(run) FROM incoming_names")
-    count_names = mapc.cur.fetchone()[0]
-    mapc.dbcon.commit()
-    if count_names > 0:
-        mapc.cur.execute("SELECT MAX(run) FROM incoming_names")
-        run = mapc.cur.fetchone()[0] + 1
-        mapc.dbcon.commit()
-    else:
-        run = 0
-
     rlog_hdr = "Log of initial f sites start, ids written & times" + \
                os.linesep
     pstart_string = "initial_f_sites: Starting up at " + \
                     str(DT.now()) + os.linesep
 
+    logfname = log_file_path + "/generateSites.log"  # log file name
     k = open(logfname, "a+")
     k.write(rlog_hdr)
     k.write(pstart_string)
@@ -43,6 +31,7 @@ def main():
     log_hdr = "Error messages from initial_f_sites" + \
               os.linesep
 
+    dberrfname = log_file_path + "/sites_dbase_error.log"
     k = open(dberrfname, "a+")
     k.write(log_hdr)
     k.close()
@@ -59,20 +48,15 @@ def main():
     names_f_hd1 = random.sample(names_f_hd, int(n_f * pro_hd * pro_hd1))
     names_f_hd2 = [item for item in names_f_hd if item not in names_f_hd1]
 
-    # Initial line in iteration_metrics
-    insert_query = "insert into iteration_metrics (run, iteration, iteration_time) values (%s, %s, %s);"
-    mapc.cur.execute(insert_query, (run, 0, DT.now()))
-    mapc.dbcon.commit()
-
     # Write them into incoming_names table
     try:
         for name in names_f:
             if name in names_f_hd2:
-                name_one = name + (run, 0, "validate")
+                name_one = name + (0, 0, "validate")
             elif name in names_f_hd1:
-                name_one = name + (run, 0, "holdout")
+                name_one = name + (0, 0, "holdout")
             else:
-                name_one = name + (run, 0, "train")
+                name_one = name + (0, 0, "train")
             insert_query = "insert into incoming_names (name, run, iteration, usage) values (%s, %s, %s, %s);"
             mapc.cur.execute(insert_query, name_one)
             mapc.dbcon.commit()
@@ -82,6 +66,7 @@ def main():
         k.write(names + os.linesep)
         k.write("initial_f_sites: Script stopping up at " + end_time + os.linesep)
         k.close()
+        return True
 
     except psycopg2.DatabaseError, err:
         print "Error updating database in initial_f_sites, rollback"
@@ -94,6 +79,7 @@ def main():
         mapc.createAlertIssue("initial_f_sites database error",
                               "Alert: Check the connection and query of database for initialing f sites." +
                               os.linesep + str(err))
+        return False
         sys.exit(1)
     finally:
         if mapc.dbcon.closed == 0:
