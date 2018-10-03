81f7088abf3629f41cb6606601adb0fb6e5ccf51
Ryan Avery <ravery@ucsb.edu>
2018-10-02 17:21:33 -0700

    added DB insert step back
    Former-commit-id: d4d9f883c847b9a0b332baca530369c63a325305 [formerly d17d79bdc80b19bfdf39322cfc262d50934d3965]
    Former-commit-id: d4d9f883c847b9a0b332baca530369c63a325305
    Former-commit-id: e64b3c499c7261dc43c390947245c182504eb6ee

--- terraform/emr.tf
+++ terraform/emr.tf
@@ -24,6 +24,13 @@ resource "aws_emr_cluster" "emr-spark-cluster" {
     args = ["spark-submit", "--master", "yarn", "--packages", "io.astraea:pyrasterframes:0.7.3-GT2,org.apache.hadoop:hadoop-aws:2.7.3,org.apache.logging.log4j:log4j-core:2.11.1", "--jars", "/opt/jars/geotrellis-backend-assembly-0.4.2.jar", "--conf", "spark.executorEnv.GDAL_DATA=/usr/local/share/gdal", "--conf", "spark.sql.shuffle.partitions=1920", "/home/hadoop/cvmlAL/run_it/run_geopyspark.py", "--probability-images", "10", "--random-seed", "42", "activemapper", "1", "ghana-aoi4-test"]
   }}
 
+  step {
+    name="run_DB_insert.py"
+    action_on_failure = "CONTINUE"
+    hadoop_jar_step {
+    jar="command-runner.jar"
+    args = ["python3","/home/hadoop/cvmlAL/run_it/run_DB_insert.py"]
+  }}
 
   termination_protection = false
   keep_job_flow_alive_when_no_steps = false
