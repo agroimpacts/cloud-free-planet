f5962337c2fe299cdfb9f3c7a117176b0dc9e1a4
Ryan Avery <ravery@ucsb.edu>
2018-10-12 14:11:02 -0700

    deleted old files
    Former-commit-id: 56f3716da7b70f57dbd9e703e4bd7f5ce7c229fe

--- certbot.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash -x
-
-certbot certonly --webroot -w /home/sandbox/mapper -d sandbox.crowdmapper.org
-
-# To renew, use a root crontab job to execute 'certbot renew' every week (fllowed by an apache restart).
