8c8082e0b7a1441a15ee762e21a6151b1269708f
Ryan Avery <ravery@ucsb.edu>
2019-06-05 22:49:38 -0700

    fixed download scripts

--- atsa-python/pyatsa_make_inputs.py
+++ atsa-python/pyatsa_make_inputs.py
@@ -6,12 +6,14 @@ import os
 from rasterio.plot import reshape_as_raster
 import json
 
-def keep_imgs_with_meta(img_paths, meta_paths, DIR):
+def keep_imgs_with_meta(img_paths, meta_paths, udm_paths, udm2_paths, DIR):
     imgs_with_meta = []
     for im in list(map(os.path.basename, sorted(img_paths))):
         for meta in list(map(os.path.basename, sorted(meta_paths))):
-            if meta[0:15] == im[0:15]:
-                imgs_with_meta.append(im)
+            for udm in list(map(os.path.basename, sorted(udm_paths))):
+                for udm2 in list(map(os.path.basename, sorted(udm2_paths))):
+                    if meta[0:15] == im[0:15] == udm[0:15] == udm2[0:15]:
+                        imgs_with_meta.append(im)
     return [os.path.join(DIR,name) for name in imgs_with_meta]
 
 def get_sun_elevation_azimuth(path):
@@ -45,23 +47,42 @@ def stack_t_series(paths, stackname):
         dst.write(stacked)
     print("Saved Time Series with " + str(len(arrs)) + " images and " + str(arrs[0].shape[2]) + " bands each")
 
+def make_arr_square(stacked_series_path):
+    """"
+    removes columns and rows with NA values to make each image square
+    """
+    arr = ski.io.imread(stacked_series_path)
+    stacked = reshape_as_raster(np.dstack(arrs))
+    img = rio.open(paths[0])
+    meta=img.profile
+    meta.update(count=len(arrs)*arrs[0].shape[2])
+    with rio.open(stackname, 'w',  **meta) as dst:
+        dst.write(stacked)
+    print("Saved Time Series with " + str(len(arrs)) + " images and " + str(arrs[0].shape[2]) + " bands each")
+    
 if __name__ == '__main__':
-    DIR = "/home/rave/cloud-free-planet/notebooks/jan-september-utm/"
-    sr_pattern = "/home/rave/cloud-free-planet/notebooks/jan-september-utm/*SR*.tif"
+    path_id = "forest"
+    DIR = os.path.join("/home/rave/cloud-free-planet/cfg/buffered_imgs/",path_id)
+    sr_pattern = "/home/rave/cloud-free-planet/cfg/buffered_aois/"+path_id+"/*SR*.tif"
     img_paths = glob.glob(sr_pattern)
     img_paths = sorted(img_paths)
-    meta_pattern = "/home/rave/cloud-free-planet/notebooks/jan-september-utm/*metadata.json"
+    meta_pattern = "/home/rave/cloud-free-planet/cfg/buffered_aois/"+path_id+"/*meta*.json"
     meta_paths = glob.glob(meta_pattern)
     meta_paths = sorted(meta_paths)
-    img_paths_with_meta = keep_imgs_with_meta(img_paths, meta_paths, DIR)
+    udm_pattern = "/home/rave/cloud-free-planet/cfg/buffered_aois/"+path_id+"/*udm_*.tif"
+    udm_paths = glob.glob(meta_pattern)
+    udm_paths = sorted(meta_paths)
+    udm2_pattern = "/home/rave/cloud-free-planet/cfg/buffered_aois/"+path_id+"/*udm2*.tif"
+    udm2_paths = glob.glob(udm2_pattern)
+    udm2_paths = sorted(udm2_paths)
+    img_paths_with_meta = keep_imgs_with_meta(img_paths, meta_paths, udm_paths, udm2_paths, DIR)
     
     angles = list(map(get_sun_elevation_azimuth, meta_paths))
-    with open(os.path.join(DIR,'angles_larger_utm.txt'), 'w') as f:
+    with open(os.path.join("/home/rave/cloud-free-planet/cfg/buffered_angles", path_id+'_angles_larger_utm.txt'), 'w') as f:
         for tup in angles:
             f.write('%s %s\n' % tup)
     
     save_blank_water_mask(img_paths_with_meta[0])
 
-    stack_t_series(img_paths_with_meta, os.path.join(DIR, "stacked_larger_utm.tif"))
-    
+    stack_t_series(img_paths_with_meta, os.path.join("/home/rave/cloud-free-planet/cfg/", path_id+"_stacked.tif"))
 
--- cfg/download_orders.sh
+++ cfg/download_orders.sh
@@ -1,11 +1,6 @@
-porder multiproc --url https://api.planet.com/compute/ops/orders/v2/82d3de7a-e35a-40bd-bc0c-4f74f8463473 --local /home/rave/cloud-free-planet/cfg/paper-aois/tseries-raw/city/
-porder multiproc --url https://api.planet.com/compute/ops/orders/v2/d8086e05-b3b5-4d08-b29a-56aed11655ef --local /home/rave/cloud-free-planet/cfg/paper-aois/tseries-raw/city/
-porder multiproc --url https://api.planet.com/compute/ops/orders/v2/08a8ec6a-41ed-4e7e-ace3-f86a75e2573a --local /home/rave/cloud-free-planet/cfg/paper-aois/tseries-raw/savanna/
-porder multiproc --url https://api.planet.com/compute/ops/orders/v2/6ccb463a-5c2e-457c-9578-5cf9853aa4df --local /home/rave/cloud-free-planet/cfg/paper-aois/tseries-raw/savanna/
-porder multiproc --url https://api.planet.com/compute/ops/orders/v2/fae6075d-f9c0-487e-8ab2-4a8afcd69505 --local /home/rave/cloud-free-planet/cfg/paper-aois/tseries-raw/ag-forestry/
-porder multiproc --url https://api.planet.com/compute/ops/orders/v2/fc42845f-1298-45b1-bc93-fd5494d58091 --local /home/rave/cloud-free-planet/cfg/paper-aois/tseries-raw/ag-forestry/
-porder multiproc --url https://api.planet.com/compute/ops/orders/v2/7bf76782-b0bb-4e9d-b17c-3abb60625ae6 --local /home/rave/cloud-free-planet/cfg/paper-aois/tseries-raw/forest/
-porder multiproc --url https://api.planet.com/compute/ops/orders/v2/593bb867-e994-4810-ba08-4b8edc930f5d --local /home/rave/cloud-free-planet/cfg/paper-aois/tseries-raw/forest/
-porder multiproc --url https://api.planet.com/compute/ops/orders/v2/ce02121a-6abf-4f94-b491-e8ab09bd9f65 --local /home/rave/cloud-free-planet/cfg/paper-aois/tseries-raw/water/
-porder multiproc --url https://api.planet.com/compute/ops/orders/v2/33ac7173-ffb9-41ca-8b60-b78613647466 --local /home/rave/cloud-free-planet/cfg/paper-aois/tseries-raw/water/
+porder multiproc --url https://api.planet.com/compute/ops/orders/v2/04b2ddba-e2a0-4009-889d-62b0bab7e109 --local /home/rave/cloud-free-planet/cfg/buffered_imgs/city/
+porder multiproc --url https://api.planet.com/compute/ops/orders/v2/2d8f2dce-85ac-4c00-9307-9b1db7a39d47 --local /home/rave/cloud-free-planet/cfg/buffered_imgs/savanna/
+porder multiproc --url https://api.planet.com/compute/ops/orders/v2/106a55e1-d93d-4f67-9e1d-a8697c3cdf48 --local /home/rave/cloud-free-planet/cfg/buffered_imgs/ag-forestry/
+porder multiproc --url https://api.planet.com/compute/ops/orders/v2/d0ff895a-ba8a-4010-af47-6099c43f168a --local /home/rave/cloud-free-planet/cfg/buffered_imgs/forest/
+porder multiproc --url https://api.planet.com/compute/ops/orders/v2/60ab7e24-ab28-47b5-bda3-b6f10c7e1fe6 --local /home/rave/cloud-free-planet/cfg/buffered_imgs/water/
 
